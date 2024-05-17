ee429db779ef0984c0c8985889f2ce1a31044709
astro <juxxtfaith@gmail.com>
2024-05-16 22:49:49 -0300

    Primeiro commit

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Desingn', id: 1, src:livro},
+    {nome: 'Cangaceiro JavaScript: Uma aventura no sertão da programação', id: 3, src:livro},
+    {nome: 'Aventureiros do Java', id: 4, src:livro},
+]
